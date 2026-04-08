79f86991bf19c21b84d91ecbd07a11f70a076621
Nathaniel A Collier <drxs3v3n@gmail.com>
2026-04-08 13:42:18 -0500

    Start implementing identifying files by hash

--- maintenance/readWinFormsResXImageListStreamer/Program.cs
+++ maintenance/readWinFormsResXImageListStreamer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -6,10 +7,21 @@ using System.Windows.Forms;
 using System.Xml.Linq;
 
 
+string blackJackProjectFolderPath = System.IO.Path.Combine(
+    AppContext.BaseDirectory,
+        "../../../../../blackjack_managed_cpp/"
+        );
+
 string resxFilePath = System.IO.Path.Combine(
-        AppContext.BaseDirectory,
-        "../../../../../blackjack_managed_cpp/Form1.resx"
+        blackJackProjectFolderPath,
+        "Form1.resx"
     );
+string cardsFolderPath = System.IO.Path.Combine(
+        blackJackProjectFolderPath,
+        "cards"
+    );
+
+List<ImageInformation> cardFilesList = LoadCardFiles(folderPath: cardsFolderPath);
 
 byte[] data = ParseDataBytesFromResource(rexFilePath: resxFilePath, key: "cardlist.ImageStream");
 
@@ -21,8 +33,13 @@ imgList.ImageStream = streamer;
 for (int i = 0; i < imgList.Images.Count; i++)
 {
     var img = imgList.Images[i];
-    Console.WriteLine($"Image[{i}]: Size={img.Size}, PixelFormat={img.PixelFormat};");
+    string resourceImageHash = GetImageHash(img);
+    var matchingCard = cardFilesList.FirstOrDefault(c => c.DataHash == resourceImageHash);
+
+    Console.WriteLine($"Image[{i}]: Size={img.Size}, PixelFormat={img.PixelFormat}, Filename: {matchingCard.FileName}");
+
 }
+
 
 
 ImageListStreamer LoadStreamer(byte[] data)
@@ -62,3 +79,41 @@ byte[] ParseDataBytesFromResource(string rexFilePath, string key)
 }
 
 
+
+string GetImageHash(System.Drawing.Image image)
+{
+    using (var ms = new System.IO.MemoryStream())
+    {
+        image.Save(ms, image.RawFormat);
+        byte[] hash = System.Security.Cryptography.SHA256.Create().ComputeHash(ms.ToArray());
+        return Convert.ToBase64String(hash);
+    }
+}
+
+
+List<ImageInformation> LoadCardFiles(string folderPath)
+{
+    var cards = new List<ImageInformation>();
+    foreach( string filePath in System.IO.Directory.EnumerateFiles(path: folderPath, searchPattern: "*.bmp", searchOption: SearchOption.AllDirectories))
+    {
+        var fileImg = System.Drawing.Image.FromFile(filePath);
+        string fileHash = GetImageHash(fileImg);
+        cards.Add(new ImageInformation
+        {
+            Index = 0,
+            FileName = System.IO.Path.GetFileName(filePath),
+            RelativePath = System.IO.Path.GetRelativePath(folderPath, filePath),
+            DataHash = fileHash
+        });
+    }
+    return cards;
+}
+
+
+class ImageInformation
+{
+    public int Index { get; set; }
+    public string FileName { get; set; }
+    public string RelativePath { get; set; }
+    public string DataHash { get; set; }
+}
